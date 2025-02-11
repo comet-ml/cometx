59d60374e672d6e0457c42c01a79c2e2f6a73b7c
Doug Blank <doug.blank@gmail.com>
2025-02-11 09:28:07 -0500

    Version 2.3.0; moved extra packages to 'all'

--- setup.py
+++ setup.py
@@ -47,10 +47,14 @@ setup_args = dict(
     long_description_content_type="text/markdown",
     install_requires=[
         "comet_ml",
-        "pillow>=11.1.0",
-        "opik",
-        "comet_mpm",
     ],
+    extras_require={
+        "all": [
+            "pillow>=11.1.0",
+            "opik",
+            "comet_mpm",
+        ],
+    },
     packages=[
         "cometx.cli",
         "cometx.tools",
